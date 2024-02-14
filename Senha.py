3185df90ad24558724935dd304fd9add1c742b78
Mike <mike.alcantara@hotmail.com>
2024-02-14 19:40:19 -0300

    Adição do chatbot que avisa sobre a tentativa de senha incorrerta

--- Senha.py
+++ Senha.py
@@ -1,10 +1,26 @@
+import pywhatkit
+import keyboard
+import time
+from datetime import datetime
 # A senha que ja foi salva pelo usuario
 secreta = str("ADM123")
-# Console esperando a senha correta ser digitada
+def correta(str):
+	if senha == secreta:
+		print("Senha correta!")
+# Duas defs preparadas com o código de comparação
+def incorreta(str):
+	if senha != secreta:
+		print("Senha incorreta!")
+	contatos = ['+5515998091511']
+	while len(contatos) >= 1:
+	 pywhatkit.sendwhatmsg(contatos[0], '!!Atenção!!, tentativa de acesso negado.', datetime.now().hour,datetime.now().minute + 2)
+del contatos[0]
+time.sleep(60)
+keyboard.press_and_release('ctrl + w')
+# Console esperando a senha ser digitada
 senha = input("Digite sua senha: ")
-# Enquanto a senha não for a correta, ele vai trazer um nova chance
-while senha != secreta:
- senha = input("Tente novamente! ")
-# Se a senha for a correta o programa segue seu fluxo
+# A mensagem vai depender da senha digitada pelo usuario
 if senha == secreta:
- print("Senha correta!")
+	correta("Senha correta")
+else:
+	incorreta("Senha incorreta!")
